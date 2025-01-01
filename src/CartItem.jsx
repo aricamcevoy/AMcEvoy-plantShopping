a21f3b18742ae049b5560c85c0bfd14a1d7d5435
aricamcevoy <arica.mcevoy@gmail.com>
2025-01-01 15:53:23 -0800

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -19,12 +19,12 @@ const CartItem = ({ onContinueShopping, onCheckoutShopping }) => {
   };
 
   const handleIncrement = (item) => {
-    dispatch(updateQuantity({ name: item.name, quantity: item.quantity += 1 }));  
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity+1 }));  
 };
 
   const handleDecrement = (item) => {
     if (item.quantity > 1) {
-    dispatch(updateQuantity({ name: item.name, quantity: item.quantity -= 1 }));
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity-1 }));
     } else {
         dispatch(removeItem(item.name));
     }
